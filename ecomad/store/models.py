ebf1270c40e50cab9cebdcdfd64bc3cb06f3148e
ypap1234pun <yaperera2@gmail.com>
2024-11-21 19:19:56 +0530

    second commit

--- ecomad/store/models.py
+++ ecomad/store/models.py
@@ -10,10 +10,34 @@ class Category(models.Model):
 
 
 class Customer(models.Model):
-  first_name =
-  last_name =
+  first_name = models.CharField(max_length=50) 
+  last_name = models.CharField(max_length=50)
+  phone = models.CharField(max_length=10)
+  email = models.EmailField(max_length=100)
+  password = models.CharField(max_length=100)
 
+def __str__(self):
+    return f'{self.first_name} {self.last_name}'
 
 class Product(models.Model):
+  name = models.CharField(max_length=100)
+  price = models.DecimalField(default=0,decimal_places=2,max_digits=8)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE,default=1)
+  description = models.CharField(max_length=250,default='',blank=True,null=True)
+  Image = models.ImageField(upload_to='uploads/product/')
+
+def __str__(self):
+    return self.name
+
 
 class Order(models.Model):
+  product = models.ForeignKey(Product, on_delete=models.CASCADE)
+  customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+  quantity = models.IntegerField(default=1,)
+  address = models.CharField(max_length=100,default='',blank=True)
+  phone = models.CharField(max_length=20,default='',blank=True)
+  date = models.DateTimeField(default=datetime.datetime.today)
+  status = models.BooleanField(default=False)
+
+  def __str__(self):
+     return self.product
