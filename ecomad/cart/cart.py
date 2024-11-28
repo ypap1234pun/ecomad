9c7b387392191bbd7d3c85d76e6a1353246cadcb
ypap1234pun <yaperera2@gmail.com>
2024-11-28 06:20:20 +0530

    5th commit

--- ecomad/cart/cart.py
+++ ecomad/cart/cart.py
@@ -15,12 +15,16 @@ class Cart():
     self.cart = cart
 
 
-  def add(self,product):
-    product_id = str(product.id)  
+  def add(self,product,quantity):
+    product_id = str(product.id) 
+    product_qty = str(quantity) 
     if product_id in self.cart:
       pass
     else:
-      self.cart[product_id] = { 'price': str(product.price)}
+      #self.cart[product_id] = { 'price': str(product.price)}
+      self.cart[product_id] = int(product_qty)
+
+
     self.session.modified = True
 
 
@@ -31,3 +35,7 @@ class Cart():
     product_ids = self.cart.keys()
     products = Product.objects.filter(id__in = product_ids)
     return products
+  
+  def get_quants(self):
+    quantities = self.cart
+    return quantities
