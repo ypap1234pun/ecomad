8da66648dc650a9489228997bd7f9dafa07335a7
ypap1234pun <yaperera2@gmail.com>
2024-11-29 05:36:34 +0530

    delete added

--- ecomad/cart/views.py
+++ ecomad/cart/views.py
@@ -24,7 +24,19 @@ def cart_add(request):
 
   
 def cart_delete(request):
-  pass
+  cart= Cart(request)
+  if request.POST.get('action') == 'post':
+    product_id = int(request.POST.get('product_id'))
+    
+
+    cart.delete(product=product_id) 
+    
+    response = JsonResponse({'product': product_id})
+    return response
+
+    
+
+  
 
 def cart_update(request):
   cart= Cart(request)
