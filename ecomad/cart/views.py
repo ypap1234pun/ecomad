30beaca1f2a564ec8f1c46ee0750b6d8cb5543fd
ypap1234pun <yaperera2@gmail.com>
2024-11-30 09:45:11 +0530

    cart messages

--- ecomad/cart/views.py
+++ ecomad/cart/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,get_object_or_404
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
+from django.contrib import messages
+
  
 def cart_summary(request):
   cart = Cart(request)
@@ -21,6 +23,8 @@ def cart_add(request):
     cart_quantity =cart.__len__()
     #response = JsonResponse({'Product.name:  ':product.name})
     response = JsonResponse({'qty ':cart_quantity})
+    messages.success(request,("Product Added to Cart"))
+
     return response
 
   
@@ -33,6 +37,7 @@ def cart_delete(request):
     cart.delete(product=product_id) 
     
     response = JsonResponse({'product': product_id})
+    messages.success(request,("Item deleted from shoping cart"))
     return response
 
     
@@ -48,6 +53,8 @@ def cart_update(request):
     cart.update(product=product_id,quantity=product_qty)
 
     response = JsonResponse({'qty': product_qty})
+    messages.success(request,("Your cart has been Updated.."))
+
     return response
     
     #return redirect ('cart_summart')
